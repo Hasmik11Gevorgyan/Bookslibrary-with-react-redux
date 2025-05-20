f6429a4592f7ceba6578bcafe06dd1b2998a96e3
Hasmik Gevorgyan <hasmikgevorgyan1990@gmail.com>
2025-05-20 10:28:34 +0400

    i have done checkbox,resetFilters, and filters

--- frontend/src/components/filter/Filter.jsx
+++ frontend/src/components/filter/Filter.jsx
@@ -1,22 +1,45 @@
- import { setTitleFilter,resetFilters,selectTitleFilter } from "../../redux/slices/filterSlice";
+ import { setTitleFilter, resetFilters, selectTitleFilter } from "../../redux/slices/filterSlice";
  import { useDispatch,useSelector } from "react-redux";
 
 
 
  const Filter = () =>{
-
+const [checked, setChecked] = useState(false);
   const dispatch = useDispatch();
   const value  = useSelector( selectTitleFilter); 
 
 
  const handleByTitle = (e) => dispatch( setTitleFilter(e.target.value))
  
- const handleResetFilters = () => dispatch(resetFilters) 
+ const handleResetFilters = () => dispatch(resetFilters()) 
  
 
   return (
-    <div className="p-4 m-4 bg-[#f2f2f2] rounded-lg shadow-lg " >
+    <div className=" flex flex-col p-4 m-4 bg-[#f2f2f2] rounded-lg shadow-lg " >
        <div>
+<div className ="flex items-center gap-2">
+  <label className ="flex items-center">
+    <input
+      type ="checkbox"
+      className ="appearance-none mr-2 cursor-pointer"
+      onChange={e => setChecked(prevState => !prevState)}
+      
+      style ={
+        checked ?{
+clipPath: "polygon(14% 44% 0% 65% 50% 100% 100% 16% 80% 0% 43% 62%)",
+        transformOrigin: "bottom left",
+        appearance: "none",
+        backgroundColor: "red",
+        }:
+        {
+          
+        }
+      }
+        />
+        <span> Only favorite</span>
+  </label>
+
+</div>
         <input
         onChange={e => handleByTitle(e)}
         value={value}
